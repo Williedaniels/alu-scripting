7895f2070e2022e11812db62ae23ca8d6cd9bb9d
Williedaniels <w.daniels@alustudent>
2023-09-06 12:30:19 +0200

    task 1

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,18 +1,3 @@
 #!/usr/bin/env ruby
 
-# Define the regular expression pattern
-regex_pattern = /^hb[t]+n$/
-
-# Check if an argument is provided
-if ARGV.empty?
-  puts "Please provide an argument."
-else
-  # Apply the regular expression to the argument
-  input = ARGV[0]
-
-  if input.match?(regex_pattern)
-    puts "Match found: #{input}"
-  else
-    puts "No match found: #{input}"
-  end
-end
+puts ARGV[0].scan(/^hb[t]+n$/)
