eb59d62cfa69764d1db28d61d2136e186fc652b7
Williedaniels <w.daniels@alustudent>
2023-09-06 21:27:54 +0200

    task 8

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -3,19 +3,23 @@
 # Define the regular expression pattern to extract sender, receiver, and flags
 regex_pattern = /\[from:([^[\]]+)\] \[to:([^[\]]+)\] \[flags:([^[\]]+)\]/
 
-# Read the log file line by line
-File.open('log_file.txt', 'r') do |file|
-  file.each_line do |line|
-    # Match the line against the regular expression
-    match = line.match(regex_pattern)
+# Check if an argument is provided
+if ARGV.empty?
+  puts "Please provide a log line as an argument."
+else
+  log_line = ARGV[0]
 
-    # Check if a match was found
-    if match
-      sender = match[1]
-      receiver = match[2]
-      flags = match[3]
-      puts "#{sender},#{receiver},#{flags}"
-    end
+  # Match the log line against the regular expression
+  match = log_line.match(regex_pattern)
+
+  # Check if a match was found
+  if match
+    sender = match[1]
+    receiver = match[2]
+    flags = match[3]
+    puts "#{sender},#{receiver},#{flags}"
+  else
+    puts "No match found for the provided log line."
   end
 end
 
