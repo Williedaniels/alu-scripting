0b71532ed80d699f3f66f6e5d2b31473199fe988
Williedaniels <w.daniels@alustudent>
2023-11-10 22:47:52 +0200

    API Advanced

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+defines the main function to get
+the top tem hot titles from the reddit
+api
+"""
+import requests
+import sys
+
+
+def top_ten(subreddit):
+    """
+    Sends a query to the reddit api to get the hot ten titles
+    for a particular subreddit
+    """
+    if subreddit is None or not isinstance(subreddit, str):
+        return (0)
+    endpoint = 'https://www.reddit.com'
+    headers = {'user-agent': 'Testapi/1.0 by glenmiracle18'}
+    params = {'limit': 10}
+    info = requests.get('{}/r/{}/hot.json'.format(
+        endpoint,
+        subreddit), headers=headers, params=params, allow_redirects=False)
+    if info.status_code == 200:
+        data_info = info.json()
+        for post in data_info.get('data').get('children'):
+            print(post.get('data').get('title'))
+    else:c
+    return (0)
